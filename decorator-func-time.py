b4cca78f6d73d68c49c0e88e54059aa287726866
YuriySaveliev <savjurnick@gmail.com>
2021-09-09 09:04:17 +0300

    Update test functions

--- decorator-func-time.py
+++ decorator-func-time.py
@@ -9,20 +9,37 @@ def calculateFibonacci(quantity):
 def calculatePrimeNumbers(quantity):
     primeNumbersSequence = []
 
-    while len(primeNumbersSequence) < quantity:
-        for number in range(1000000):
-            if number
+    for number in range(1000000):
+       if _isPrimeNumber(number):
+           primeNumbersSequence.append(number)
+       if len(primeNumbersSequence) == quantity:
+           break
 
-    return quantity
+    return primeNumbersSequence
+
+def _isPrimeNumber(number):
+    counter = number
+    isPrime = False
+
+    while counter > 1:
+        counter = counter - 1
+        if number % counter == 0:
+            break
+        if counter == 2:
+            isPrime = True
+
+    return isPrime
 
 def raiseExceptionFunction():
     raise NameError('Error')
 
-'''class FuncTimeDecorator:
+class FuncTimeDecorator:
     def __init__(self):
         self.file_name = file_name
 
     def log_time(self):
-        with open('func_time.txt', 'w') as log_file:'''
-
-print(calculateFibonacci(100))
+        #with open('func_time.txt', 'w') as log_file:
+        pass
+#print(calculateFibonacci(10))
+#print(_isPrimeNumber(1))
+print(calculatePrimeNumbers(100))
